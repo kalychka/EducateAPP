767124a18ea0b317b5b4963e19a67b2c63473218
randy taylor <randytaylor.fckoff@gmail.com>
2021-10-30 08:39:23 +0500

    Добавление регистрации пользователя

---
+++ EducateAPP/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducateAPP.ViewModels
+{
+    public class RegisterViewModel
+    {
+
+        [Required(ErrorMessage = "Введите e-mail")]
+        [Display(Name = "e-mail")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Введите фамилию")]
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Введите отчество")]
+        [Display(Name = "Отчество")]
+        public string Patronymic { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль")]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Повторите ввод пароля")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [Display(Name = "Подтвердите пароль")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirm { get; set; }
+    }
+}
