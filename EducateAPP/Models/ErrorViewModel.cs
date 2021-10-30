c3cb881afbd8b9b88eb85cce7055ce49e8e50165
randy <randy@KALYCHKA>
2021-10-30 07:02:11 +0500

    Добавьте файлы проекта.

---
+++ EducateAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EducateAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
